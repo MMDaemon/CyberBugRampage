e662a005eec17791ecfe964183a884809ff8580e
MMDaemon <mettenleitermatthias@web.de>
2016-10-20 16:27:49 +0200

    Optimized mouse-movement of camera

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -10,20 +10,18 @@ public class LookAtPlayer : MonoBehaviour
 	[SerializeField]
 	float heightPercentage = 0.8f;
 	[SerializeField]
-	float speedH = 2.0f;
+	float speedV = 2.0f;
 	[SerializeField]
-	float cameraHeight = 0.2f;
+	float speedH = 2.0f;
 
 	private Transform _player;
-	private Transform _cameraPos;
 	private CapsuleCollider _playerCollider;
-	
+
 
 	// Use this for initialization
 	void Start()
 	{
 		GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-		_cameraPos = this.transform.GetChild(0);
 		_player = playerObject.transform;
 		_playerCollider = playerObject.GetComponent<CapsuleCollider>();
 	}
@@ -33,17 +31,19 @@ public class LookAtPlayer : MonoBehaviour
 	{
 		Vector3 playerShoulder = _player.position + _playerCollider.transform.up * _playerCollider.height * heightPercentage;
 
-		_cameraPos.LookAt(playerShoulder);
+		transform.LookAt(playerShoulder);
 
-		transform.eulerAngles = new Vector3(20, _cameraPos.transform.rotation.eulerAngles.y, 0);
-		float currentDistance = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(playerShoulder.x, playerShoulder.z));
-		if (currentDistance != distance)
+		float currentDistance = Vector3.Distance(transform.position, playerShoulder);
+		transform.position = transform.position + transform.forward * (currentDistance - distance);
+		Vector3 rotationX = new Vector3(speedV * Input.GetAxis("Mouse Y"), 0.0f, 0.0f);
+		if ((transform.eulerAngles - rotationX).x < 60.0f || (transform.eulerAngles - rotationX).x > 300.0f)
 		{
-			transform.position = transform.position + new Vector3(transform.forward.x, 0, transform.forward.z) * (currentDistance - distance);
+			transform.eulerAngles -= rotationX;
 		}
+		Vector3 rotationY = new Vector3(0.0f, speedH * Input.GetAxis("Mouse X"), 0.0f);
+		transform.eulerAngles += rotationY;
 
-		this.transform.eulerAngles += new Vector3(0.0f, speedH * Input.GetAxis("Mouse X"), 0.0f);
 
-		this.transform.position = playerShoulder - new Vector3(transform.forward.x, -cameraHeight, transform.forward.z) * distance;
+		this.transform.position = playerShoulder - transform.forward * distance;
 	}
 }
