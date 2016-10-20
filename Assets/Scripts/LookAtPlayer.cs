ea7a3200916f549fc1e92e082f007ae640ed8115
MMDaemon <mettenleitermatthias@web.de>
2016-10-20 15:45:51 +0200

    Added camerarotation with mousecontroll

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -8,18 +8,16 @@ public class LookAtPlayer : MonoBehaviour
 	[SerializeField]
 	float distance = 2.0f;
 	[SerializeField]
-	float height = 0.8f;
+	float heightPercentage = 0.8f;
 	[SerializeField]
 	float speedH = 2.0f;
 	[SerializeField]
-	float speedV = 2.0f;
+	float cameraHeight = 0.2f;
 
 	private Transform _player;
 	private Transform _cameraPos;
-	CapsuleCollider _playerCollider;
-
-	private float yaw = 0.0f;
-	private float pitch = 0.0f;
+	private CapsuleCollider _playerCollider;
+	
 
 	// Use this for initialization
 	void Start()
@@ -33,7 +31,7 @@ public class LookAtPlayer : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		Vector3 playerShoulder = _player.position + _playerCollider.transform.up * _playerCollider.height * height;
+		Vector3 playerShoulder = _player.position + _playerCollider.transform.up * _playerCollider.height * heightPercentage;
 
 		_cameraPos.LookAt(playerShoulder);
 
@@ -41,12 +39,11 @@ public class LookAtPlayer : MonoBehaviour
 		float currentDistance = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(playerShoulder.x, playerShoulder.z));
 		if (currentDistance != distance)
 		{
-			transform.position = transform.position + new Vector3( transform.forward.x , 0, transform.forward.z)*(currentDistance - distance);
+			transform.position = transform.position + new Vector3(transform.forward.x, 0, transform.forward.z) * (currentDistance - distance);
 		}
 
-		//yaw += speedH * Input.GetAxis("Mouse X");
-		//this.transform.eulerAngles = new Vector3(20.0f, yaw, 0.0f);
+		this.transform.eulerAngles += new Vector3(0.0f, speedH * Input.GetAxis("Mouse X"), 0.0f);
 
-		//this.transform.position = playerShoulder - this.transform.forward*distance;
+		this.transform.position = playerShoulder - new Vector3(transform.forward.x, -cameraHeight, transform.forward.z) * distance;
 	}
 }
