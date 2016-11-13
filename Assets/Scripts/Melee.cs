2d3d03ceb3f0d4bf9f4dbdb081bcdf375ed937cb
MMDaemon <mettenleitermatthias@web.de>
2016-11-13 16:52:53 +0100

    Added hit animation with collision.

---
+++ Assets/Scripts/Melee.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Melee : MonoBehaviour
+{
+
+	public Collider RightHandCollider;
+
+	private Animator _animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_animator = GetComponent<Animator>();
+		RightHandCollider.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			StartAttack();
+		}
+		else if (!_animator.GetCurrentAnimatorStateInfo(1).IsName("Torso Layer.Slap"))
+		{
+			FinishAttack();
+		}
+		else
+		{
+			_animator.SetBool("Hit", false);
+		}
+	}
+
+	private void StartAttack()
+	{
+		_animator.SetBool("Hit", true);
+		RightHandCollider.enabled = true;
+	}
+
+	private void FinishAttack()
+	{
+		RightHandCollider.enabled = false;
+	}
+}
